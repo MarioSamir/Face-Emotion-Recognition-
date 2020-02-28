d0a999f35daad3439fcfa59f41d3e875b4a6c0f3
MarioSamir <mariosamir385@gmail.com>
2020-02-28 02:26:01 +0200

    Testing in real time

--- faceDetection.py
+++ faceDetection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from tensorflow import keras
 faceCascade=cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
 """imgPath = "WholeImages/images (6).jpeg"
 img = cv2.imread(imgPath)
@@ -16,15 +17,38 @@ cv2.imshow("image",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()  
 """
+modelPath = "Models/TSModel2"
+model = keras.models.load_model(modelPath)
+cap = cv2.VideoCapture(0)
 
-cap=cv2.VideoCapture(0)
+def preprocessingImageToClassifier(image=None,imageSize=48,mu=92.31604903840567,std=70.85156431910688):
+    image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    image = cv2.resize(image,(imageSize,imageSize))
+    image = (image - mu) / std
+    image = image.reshape(1,imageSize,imageSize,1)
+    return image
+
+
+mu = 130.2243228911731
+std = 74.46392763687916
 while True:
-    _,frame=cap.read()
+    _,frame = cap.read()
     if _ == True:
-	    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	    faces=faceCascade.detectMultiScale(gray,1.5,5)
-	    for (x,y,w,h) in faces:
-		    frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+	    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	    faces = faceCascade.detectMultiScale(gray,1.5,5)
+	    for (x, y, w, h) in faces:
+	    	face = frame[y:(y+h) , x:(x+w)]
+	    	face = preprocessingImageToClassifier(image = face,
+                                    imageSize = 48,
+                                    mu = mu,
+                                    std = std)
+	    	pred = model.predict(face) * 100
+	    	print("Angry = "+str(pred[0][0])+"%")
+	    	print("Happy = "+str(pred[0][1])+"%")
+	    	print("Sad = "+str(pred[0][2])+"%")
+	    	print("Surprice = "+str(pred[0][3])+"%")
+	    	print("-----------------------------")
+	    	frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 	    cv2.imshow("video",frame)
 
 	    if cv2.waitKey(20) & 0xFF ==ord('q'):
